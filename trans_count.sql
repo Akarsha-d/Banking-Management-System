48c1590a59190ad2802437f94cf1cbc8bf42b5df
Akankshacode <diggikar.a@northeastern.edu>
2021-04-26 21:23:05 -0400

    Adding reports

---
+++ trans_count.sql
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<displays>
+<display id="0ff5c3fa-0179-1000-8001-c0a80099ae38" type="" style="Table" enable="true">
+	<name><![CDATA[Loan taken by customer]]></name>
+	<description><![CDATA[Number of customers who have taken loan]]></description>
+	<tooltip><![CDATA[]]></tooltip>
+	<drillclass><![CDATA[]]></drillclass>
+	<CustomValues>
+		<TYPE><![CDATA[horizontal]]></TYPE>
+	</CustomValues>
+	<query>
+		<sql><![CDATA[select count(*) as LOAN_CUST_COUNT from customer e where e.cust_id in (select cust_id from customer_loan)]]></sql>
+	</query>
+		<pdf version="VERSION_1_7" compression="CONTENT">
+			<docproperty title="null" author="null" subject="null" keywords="null" />
+			<cell toppadding="2" bottompadding="2" leftpadding="2" rightpadding="2" horizontalalign="LEFT" verticalalign="TOP" wrap="true" />
+			<column>
+				<heading font="null" size="10" style="NORMAL" color="-16777216" rowshading="-1" labeling="FIRST_PAGE" />
+				<footing font="null" size="10" style="NORMAL" color="-16777216" rowshading="-1" labeling="NONE" />
+				<blob blob="NONE" zip="false" />
+			</column>
+			<table font="null" size="10" style="NORMAL" color="-16777216" userowshading="false" oddrowshading="-1" evenrowshading="-1" showborders="true" spacingbefore="12" spacingafter="12" horizontalalign="LEFT" />
+			<header enable="false" generatedate="false">
+				<data>
+				null				</data>
+			</header>
+			<footer enable="false" generatedate="false">
+				<data value="null" />
+			</footer>
+			<pagesetup papersize="LETTER" orientation="1" measurement="in" margintop="1.0" marginbottom="1.0" marginleft="1.0" marginright="1.0" />
+		</pdf>
+</display>
+</displays>
