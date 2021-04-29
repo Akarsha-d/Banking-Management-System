984b767a5728d1eb6a90c063feef1e512853513f
Akankshacode <diggikar.a@northeastern.edu>
2021-04-29 15:53:07 -0400

    PURGE_DB
    Uploading purge DB

---
+++ purge_DB.sql
@@ -0,0 +1,19 @@
+drop table "DESIGNATION" cascade constraints PURGE;
+drop table "LOAN" cascade constraints PURGE;
+drop table "CARD_SERVICES" cascade constraints PURGE;
+drop table "ADDRESS" cascade constraints PURGE;
+drop table "BRANCH" cascade constraints PURGE;
+drop table "SAVINGS_ACCOUNT" cascade constraints PURGE;
+drop table "CHECKING_ACCOUNT" cascade constraints PURGE;
+drop table "TRANSACTIONS" cascade constraints PURGE;
+drop table "EMPLOYEE" cascade constraints PURGE;
+drop table "CUSTOMER" cascade constraints PURGE;
+drop table "CUSTOMER_INSURANCE" cascade constraints PURGE;
+drop table "EMPLOYEE_INSURANCE" cascade constraints PURGE;
+drop table "EMPLOYEE_CARD_SERVICES" cascade constraints PURGE;
+drop table "CUSTOMER_CARD_SERVICES" cascade constraints PURGE;
+drop table "CUSTOMER_LOAN" cascade constraints PURGE;
+drop table "EMPLOYEE_LOAN" cascade constraints PURGE;
+
+
+
